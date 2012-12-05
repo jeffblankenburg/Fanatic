7957d09c4b6041ca50900a4bea4ee547273850a8
Jeff Blankenburg <github@jeffblankenburg.com>
2012-12-05 15:19:18 -0500

    Buncha updates and images

---
+++ FanaticWP/FanaticWP/Controls/LiveTileType2.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media.Animation;
+
+namespace FanaticWP.Controls
+{
+    public partial class LiveTileType2 : UserControl
+    {
+        public LiveTileType2()
+        {
+            InitializeComponent();
+            Storyboard anim = (Storyboard)FindName("liveTileAnim1_Part1");
+            anim.Begin();
+        }
+
+        private void liveTileAnim1_Part1_Completed_1(object sender, EventArgs e)
+        {
+            Storyboard anim = (Storyboard)FindName("liveTileAnim1_Part2");
+            anim.Begin();
+        }
+
+        private void liveTileAnim1_Part2_Completed_1(object sender, EventArgs e)
+        {
+            Storyboard anim = (Storyboard)FindName("liveTileAnim2_Part1");
+            anim.Begin();
+        }
+        private void liveTileAnim2_Part1_Completed_1(object sender, EventArgs e)
+        {
+            Storyboard anim = (Storyboard)FindName("liveTileAnim2_Part2");
+            anim.Begin();
+        }
+
+        private void liveTileAnim2_Part2_Completed_1(object sender, EventArgs e)
+        {
+            Storyboard anim = (Storyboard)FindName("liveTileAnim1_Part1");
+            anim.Begin();
+        }
+    }
+}
